c4ab949cd89622111f4f85dcb9bd61a6680450f1
etuviana <etuviana@gmail.com>
2021-10-25 11:49:04 +0300

    add driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); //Time unit and   driver.manage().window().minimize();

--- src/test/java/FullScreen.java
+++ src/test/java/FullScreen.java
@@ -2,11 +2,17 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class FullScreen {
     public static void main(String[] args) {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); //Time unit
+
         driver.get("https://www.google.co.il");
         driver.manage().window().fullscreen();
+        driver.manage().window().minimize();
     }
 }
